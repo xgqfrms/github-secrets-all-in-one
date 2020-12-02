3812d99c04e4945f63622a38fc6111c381acae77
xgqfrms <xgqfrms@outlook.com>
2020-12-03 01:25:46 +0800

    Update entrypoint.sh

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh -l
 
 sh -c "echo Github Action, my name is $INPUT_MY_NAME"
+
+
+sh -c "echo Github Action, my name is $INPUT_TOKEN"
+
+
+sh -c "echo Github Action, my name is $INPUT_access_token"
