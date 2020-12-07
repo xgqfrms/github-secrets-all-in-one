7a45a42d5cb317185841f24b96d503428e1ac182
xgqfrms <xgqfrms@outlook.com>
2020-12-07 16:39:44 +0800

    m === °C, u === °F (default)
    https://www.cnblogs.com/xgqfrms/p/14097237.html

--- weather.sh
+++ weather.sh
@@ -6,7 +6,10 @@ set -eux
 # 设置 env
 LANGUAGE="zh-CN"
 CITY=Shanghai
+# m === °C
 UNIT=m
+# u === °F (default)
+# UNIT=u
 UA="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4343.0 Safari/537.36"
 
 # 文档见 wttr.in
@@ -14,5 +17,6 @@ curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   -o result.html \
-  wttr.in/$CITY
+  wttr.in/$CITY?$UNIT
+  # wttr.in/$CITY
   # wttr.in/$CITY?format=4\&$UNIT
